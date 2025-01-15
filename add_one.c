0a830f26948267aebf0352e17ed268c302f7df9f
Gao <magao@ieng6-202.ucsd.edu>
2025-01-15 15:41:27 -0800

    modified add_one to handle pass by reference

--- add_one.c
+++ add_one.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-void add_one(int a) {
+void add_one(int *a) {
     a += 1;
+    printf("From add_one, num1 as pointer: %p\n", a);
+    printf("From add_one, num1 as integer: %d\n", a);
+
     return;
 }
 
 int main() {
     int num1 = 5;
-    add_one(num1);
+    printf("num1 Address: %p\n", &num1);
+    add_one(&num1);
+    printf("num1 Address: %p\n", &num1);
     printf("num1: %d\n", num1);
     return 0;
 }
